a42d8ae334f070d32a7b10b3f1ba22cddddf84ad
itommen <itommen@gmail.com>
2020-12-11 19:00:44 +0200

    changed paramter name to card (by the api)

--- onestep/src/Walks/SingleWalk/index.js
+++ onestep/src/Walks/SingleWalk/index.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
-
 import { useParams } from 'react-router-dom';
+
+import Card from './Card';
 
 const SingleWalk = () => {
   const { id } = useParams();
 
-  const [paramters, setParamters] = useState([]);
+  const [cards, setCards] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const loadParamters = async () => {
+    const loadCards = async () => {
       setIsLoading(true);
       try {
-        const { data } = await axios.get(`api/v3/take_home_project/my_walks/${id}`);
-        // setParamters(result);
+        const { data: { cards } } = await axios.get(`api/v3/take_home_project/my_walks/${id}`);
+        setCards(cards);
         setIsLoading(false);
         debugger
       }
@@ -26,10 +27,16 @@ const SingleWalk = () => {
       }
     };
 
-    loadParamters();
+    loadCards();
   }, []);
 
-  return <div>Single walk, id: {id}</div>
+  return <div>
+    <div>Single walk, id: {id}</div>
+    <div>
+      {/* TODO: Key temp */}
+      {cards.map((x,index) => <Card key={index} {...x } />)}
+    </div>
+  </div>
 };
 
 export default SingleWalk;
